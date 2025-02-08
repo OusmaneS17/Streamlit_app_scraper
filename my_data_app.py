e3f1a6bd01b3c84fecc210b500a3b154099529b1
Ousmane SOW <126756733+OusmaneS17@users.noreply.github.com>
2025-02-08 15:39:43 +0000

    Update my_data_app.py

--- my_data_app.py
+++ my_data_app.py
@@ -7,8 +7,12 @@ import time
 from PIL import Image
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive"
 }
+
 
 
 
